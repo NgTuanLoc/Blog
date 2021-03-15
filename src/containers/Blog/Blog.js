e45e4eef3584e5fd918c84108ef0ca6c77c0eee8
NgTuanLoc <18521011@gm.uit.edu.vn>
2021-03-15 10:50:00 +0700

    adding lazy loading for newpost cmp

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,9 +1,12 @@
 import React, { Component } from "react";
 import Posts from "./Posts/Posts";
-import NewPost from "./NewPost/NewPost";
-
-import { Route, NavLink, Switch, Redirect } from "react-router-dom";
+import asyncComponent from "../../hoc/asyncComponent";
+import { Route, NavLink, Switch, Redirect} from "react-router-dom";
 import "./Blog.css";
+
+const AsyncNewPost = asyncComponent(() => {
+  return import("./NewPost/NewPost");
+});
 
 class Blog extends Component {
   render() {
@@ -40,9 +43,10 @@ class Blog extends Component {
           </nav>
         </header>
         <Switch>
-          <Route path="/new-post" component={NewPost} />
+          <Route path="/new-post" component={AsyncNewPost} />
           <Route path="/posts" component={Posts} />
-          <Redirect from='/' to='/posts'/>
+          <Redirect from="/" to="/posts" />
+          <Route render={() => <h1>404 Not Found </h1>} />
         </Switch>
       </div>
     );
--- src/containers/Blog/NewPost/NewPost.js
+++ src/containers/Blog/NewPost/NewPost.js
@@ -21,9 +21,7 @@ class NewPost extends Component {
       .post("https://jsonplaceholder.typicode.com/posts/", post)
       .then((response) => {
         console.log(response);
-        this.setState({
-          submitted: true
-        })
+        this.props.history.push('/posts/')
       });
   };
 
